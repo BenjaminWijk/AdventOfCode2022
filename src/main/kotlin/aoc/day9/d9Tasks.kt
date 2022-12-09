bbba5ac37c753072388d43aa238c486cd8199493
Benjamin Wijk <benjamin.wijk@cevt.se>
2022-12-09 17:13:50 +0100

    Day9a + unfinished B

---
+++ src/main/kotlin/aoc/day9/d9Tasks.kt
@@ -0,0 +1,31 @@
+package main.kotlin.aoc.day9
+
+import main.kotlin.util.printIt
+import util.FileHandler
+
+class D9Tasks(val input: List<String>) {
+
+    fun getNumberOfTailPositions(ropeLength: Int) : Int{
+        val rope = Rope(ropeLength)
+        input.forEach {
+            val direction = Direction.of(it[0])
+            val repetitions = it.substring(2).toInt()
+
+            rope.moveHead(direction,repetitions)
+        }
+
+        return rope.tailTrail.size
+    }
+
+}
+
+fun main(){
+
+    val d9Tasks = D9Tasks(FileHandler.getLinesFromFile("day9.txt"))
+    d9Tasks.getNumberOfTailPositions(2).printIt()
+    d9Tasks.getNumberOfTailPositions(10).printIt()
+
+    val d9SampleTask = D9Tasks(FileHandler.getLinesFromFile("day9Sample.txt"))
+    d9SampleTask.getNumberOfTailPositions(10).printIt("sample result: ")
+
+}
